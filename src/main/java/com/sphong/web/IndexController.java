f0e204f9aa91b9d6d272e8aa3435f05a6e1bd180
홍성표 <sphong0417@gmail.com>
2020-01-07 23:59:21 +0900

    Add UI with Mustache

--- src/main/java/com/sphong/web/IndexController.java
+++ src/main/java/com/sphong/web/IndexController.java
@@ -1,13 +1,37 @@
 package com.sphong.web;
 
+import com.sphong.web.dto.PostsResponseDto;
+import com.sphong.web.dto.PostsUpdateRequestDto;
+import com.sphong.web.service.PostService;
+import org.hibernate.annotations.Parameter;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 @Controller
 public class IndexController {
 
+    @Autowired
+    private PostService postService;
+
     @GetMapping("/")
-    public String index() {
+    public String index(Model model) {
+        model.addAttribute("posts",postService.findAllDesc());
         return "index";
     }
+
+    @GetMapping("posts/save")
+    public String postsSave() {
+        return "posts-save";
+    }
+
+    @GetMapping("posts/update/{id}")
+        public String postsUpdate(@PathVariable Long id, Model mode) {
+        PostsResponseDto responseDto = postService.findById(id);
+        mode.addAttribute("post",responseDto);
+            return "posts-update";
+        }
 }
+
